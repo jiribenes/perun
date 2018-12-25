766c27470c9c04dd7e7a536433fe391437fdbe52
Jiří Beneš <mail@jiribenes.com>
2018-12-25 12:52:09 +0100

    Moved Range into the 'parser' module

--- src/parser/range.hpp
+++ src/parser/range.hpp
@@ -1,26 +1,27 @@
-#ifndef PERUN_AST_RANGE_HPP
-#define PERUN_AST_RANGE_HPP
+#ifndef PERUN_PARSER_RANGE_HPP
+#define PERUN_PARSER_RANGE_HPP
 
 #include <string>
 
-#include "node.hpp"
+#include "../ast/node.hpp"
 
 namespace perun {
-namespace ast {
+namespace parser {
 
 /// Range of token indexes representing a part of the source code
 struct Range {
     constexpr Range(size_t first, size_t last) : first(first), last(last) {}
-    explicit constexpr Range(size_t tokenIndex) : first(tokenIndex), last(tokenIndex) {}
+    explicit constexpr Range(size_t tokenIndex)
+        : first(tokenIndex), last(tokenIndex) {}
 
     size_t first, last; // first and last token index of this source range
 
-    static Range get(const Node& node) noexcept {
+    static Range get(const ast::Node& node) noexcept {
         return Range(node.firstTokenIndex(), node.lastTokenIndex());
     }
 };
 
-} // namespace ast
+} // namespace parser
 } // namespace perun
 
-#endif // PERUN_AST_RANGE_HPP
+#endif // PERUN_PARSER_RANGE_HPP
